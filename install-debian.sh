d311fa812a85a8a17d0e34152544d762ec79ab92
Francisco <francisco_amaro_rejon@hotmail.com>
2026-03-25 09:20:09 -0600

    add commands to install brave and remove firefox

--- install-debian.sh
+++ install-debian.sh
@@ -3,4 +3,21 @@
 sudo apt update
 
 # Instalar entorno de escritorio y de inicio de sesion
-sudo apt install -y gnome-core gdm3 && sudo reboot
+sudo apt install -y gnome-core gdm3
+
+# Instar brave browser
+sudo apt install -y curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+
+sudo apt update
+
+sudo apt install -y brave-browser
+
+# Eliminar firefox
+sudo apt remove -y firefox-esr
+
+# Reiniciar SO
+sudo reboot
