f0d00ad040cb2dc56773437dd8ffca08a3f8b92d
Francisco <francisco_amaro_rejon@hotmail.com>
2026-03-25 09:24:17 -0600

    change commands order

--- install-debian.sh
+++ install-debian.sh
@@ -2,18 +2,16 @@
 # Actualizar repositorios
 sudo apt update
 
+# Instalar curl
+sudo apt install -y curl
+
 # Instalar entorno de escritorio y de inicio de sesion
 sudo apt install -y gnome-core gdm3
 
 # Instar brave browser
-sudo apt install -y curl
-
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
-
 sudo apt update
-
 sudo apt install -y brave-browser
 
 # Eliminar firefox
